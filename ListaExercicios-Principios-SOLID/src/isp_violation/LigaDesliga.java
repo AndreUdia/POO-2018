241dd9913b25b0aa2cbd91eb56fc2eb4f4217812
André Vasconcelos Souto <soutom@yahoo.com.br>
2018-04-26 00:07:39 -0300

    Add files via upload

---
+++ ListaExercicios-Principios-SOLID/src/isp_violation/LigaDesliga.java
@@ -0,0 +1,16 @@
+package isp_violation;
+
+public interface LigaDesliga {
+
+    void ligar();
+
+    void desligar();
+
+    void ligarRadio();
+
+    void desligarRadio();
+
+    void ligarCamera();
+
+    void desligarCamera();
+}
